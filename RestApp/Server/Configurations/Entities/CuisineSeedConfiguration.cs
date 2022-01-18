8cd1f4d28c60f1d2b79ed4441b709b353a3e1f17
IVAN PANG YANG KAI 2002719I <2002719I@student.tp.edu.sg>
2022-01-18 11:16:43 +0800

    Menu

---
+++ RestApp/Server/Configurations/Entities/CuisineSeedConfiguration.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RestApp.Shared.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApp.Server.Configurations.Entities
+{
+    public class CuisineSeedConfiguration : IEntityTypeConfiguration<Cuisine>
+    {
+        public void Configure(EntityTypeBuilder<Cuisine> builder)
+        {
+            builder.HasData(
+                new Cuisine
+                {
+                    Name = "French"
+                },
+                new Cuisine
+                {
+                    Name = "Japanese"
+                },
+                new Cuisine
+                {
+                    Name = "Indian"
+                },
+                new Cuisine
+                {
+                    Name = "Thai"
+                },
+                new Cuisine
+                {
+                    Name = "Chinese"
+                },
+                new Cuisine
+                {
+                    Name = "American"
+                },
+                new Cuisine
+                {
+                    Name = "Italian"
+                }
+                );
+        }
+    }
+}
---
+++ RestApp/Server/Configurations/Entities/FoodSeedConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RestApp.Shared.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApp.Server.Configurations.Entities
+{
+    public class FoodSeedConfiguration : IEntityTypeConfiguration<Food>
+    {
+        public void Configure(EntityTypeBuilder<Food> builder)
+        {
+            builder.HasData(
+                new Food
+                {
+                    Name = "pdf/Italian Vecchio Menu.pdf"
+                },
+                new Food
+                {
+                    Name = ""
+                },
+                new Food
+                {
+                    Name = ""
+                },
+                new Food
+                {
+                    Name = ""
+                },
+                new Food
+                {
+                    Name = ""
+                }
+                );
+        }
+    }
+}
